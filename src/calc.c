7be28d494b73bcc0671cc007aabd65a8d355549e
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-05-13 10:29:03 +0200

    Good working base

--- src/calc.c
+++ src/calc.c
@@ -44,160 +44,169 @@ G_MODULE_EXPORT Mode mode;
  */
 typedef struct
 {
-    char *last_result;
+    char* last_result;
     unsigned int array_length;
-} MYPLUGINModePrivateData;
+    GPtrArray* history;
+} CALCModePrivateData;
 
-
-static void get_myplugin (  Mode *sw )
+static void get_calc(Mode* sw)
 {
     /**
      * Get the entries to display.
      * this gets called on plugin initialization.
      */
-    MYPLUGINModePrivateData *pd = (MYPLUGINModePrivateData *) mode_get_private_data ( sw );
+    CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(sw);
     pd->array_length = 1;
     pd->last_result = g_strdup("");
+    pd->history = g_ptr_array_new();
 }
 
-
-static int myplugin_mode_init ( Mode *sw )
+static int calc_mode_init(Mode* sw)
 {
     /**
      * Called on startup when enabled (in modi list)
      */
-    if ( mode_get_private_data ( sw ) == NULL ) {
-        MYPLUGINModePrivateData *pd = g_malloc0 ( sizeof ( *pd ) );
-        mode_set_private_data ( sw, (void *) pd );
+    if (mode_get_private_data(sw) == NULL) {
+        CALCModePrivateData* pd = g_malloc0(sizeof(*pd));
+        mode_set_private_data(sw, (void*)pd);
         // Load content.
-        get_myplugin ( sw );
+        get_calc(sw);
     }
     return TRUE;
 }
-static unsigned int myplugin_mode_get_num_entries ( const Mode *sw )
+static unsigned int calc_mode_get_num_entries(const Mode* sw)
 {
-    const MYPLUGINModePrivateData *pd = (const MYPLUGINModePrivateData *) mode_get_private_data ( sw );
+    const CALCModePrivateData* pd = (const CALCModePrivateData*)mode_get_private_data(sw);
     return pd->array_length;
 }
 
-static ModeMode myplugin_mode_result ( Mode *sw, int mretv, char **input, unsigned int selected_line )
+static ModeMode calc_mode_result(Mode* sw, int mretv, char** input, unsigned int selected_line)
 {
-    ModeMode           retv  = MODE_EXIT;
-    MYPLUGINModePrivateData *pd = (MYPLUGINModePrivateData *) mode_get_private_data ( sw );
-    if ( mretv & MENU_NEXT ) {
+    ModeMode retv = MODE_EXIT;
+    CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(sw);
+    if (mretv & MENU_NEXT) {
+        g_message("next");
         retv = NEXT_DIALOG;
-    } else if ( mretv & MENU_PREVIOUS ) {
+    } else if (mretv & MENU_PREVIOUS) {
+        g_message("previous");
         retv = PREVIOUS_DIALOG;
-    } else if ( mretv & MENU_QUICK_SWITCH ) {
-        retv = ( mretv & MENU_LOWER_MASK );
-    } else if ( ( mretv & MENU_OK ) ) {
+    } else if (mretv & MENU_QUICK_SWITCH) {
+        g_message("switch");
+        retv = (mretv & MENU_LOWER_MASK);
+    } else if ((mretv & MENU_OK)) {
+        g_message("enter");
+        g_ptr_array_add(pd->history, (gpointer) pd->last_result);
         retv = RELOAD_DIALOG;
-    } else if ( ( mretv & MENU_ENTRY_DELETE ) == MENU_ENTRY_DELETE ) {
+    } else if ((mretv & MENU_ENTRY_DELETE) == MENU_ENTRY_DELETE) {
+        g_message("delete");
         retv = RELOAD_DIALOG;
     }
-    g_message("result");
     return retv;
 }
 
-static void myplugin_mode_destroy ( Mode *sw )
+static void calc_mode_destroy(Mode* sw)
 {
-    MYPLUGINModePrivateData *pd = (MYPLUGINModePrivateData *) mode_get_private_data ( sw );
-    if ( pd != NULL ) {
-        g_free ( pd );
-        mode_set_private_data ( sw, NULL );
+    CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(sw);
+    if (pd != NULL) {
+        g_free(pd);
+        mode_set_private_data(sw, NULL);
     }
 }
 
-static char *_get_display_value ( const Mode *sw, unsigned int selected_line, G_GNUC_UNUSED int *state, G_GNUC_UNUSED GList **attr_list, int get_entry )
+static char* calc_get_display_value(const Mode* sw, unsigned int selected_line, int* state, G_GNUC_UNUSED GList** attr_list, int get_entry)
 {
-    MYPLUGINModePrivateData *pd = (MYPLUGINModePrivateData *) mode_get_private_data ( sw );
+    CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(sw);
 
-    // Only return the string if requested, otherwise only set state.
-    return get_entry ? g_strdup(pd->last_result): NULL;
+    if (!get_entry) {
+        return NULL;
+    }
+    g_message("selected line: %i", selected_line);
+    /* char* lol = g_ptr_array_index(pd->history, selected_line); */
+    /* g_message("array: %s", lol); */
+    return g_strdup("lol");
+    return g_strdup(g_ptr_array_index(pd->history, selected_line));
 }
 
-/**
- * @param sw The mode object.
- * @param tokens The tokens to match against.
- * @param index  The index in this plugin to match against.
- *
- * Match the entry.
- *
- * @param returns try when a match.
- */
-static int myplugin_token_match ( const Mode *sw, rofi_int_matcher **tokens, unsigned int index )
+static int calc_token_match(const Mode* sw, rofi_int_matcher** tokens, unsigned int index)
 {
-    MYPLUGINModePrivateData *pd = (MYPLUGINModePrivateData *) mode_get_private_data ( sw );
-
-    // Call default matching function.
-    return helper_token_match ( tokens, pd->last_result);
+    /* CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(sw); */
+    return TRUE;
 }
 
-static void process_cb(GObject *source_object, GAsyncResult *res, gpointer user_data)
+static void process_cb(GObject* source_object, GAsyncResult* res, gpointer user_data)
 {
     g_autoptr(GError) error = NULL;
-    MYPLUGINModePrivateData *pd = (MYPLUGINModePrivateData *) mode_get_private_data ( user_data );
-    g_message("cb");
-    GSubprocess* process = (GSubprocess*) source_object;
-    g_subprocess_wait_finish(process, res, &error);
+    CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(user_data);
+    GSubprocess* process = (GSubprocess*)source_object;
+    g_subprocess_wait_check_finish(process, res, &error);
 
-    if (error != NULL)
-    {
-        g_error ("Process errored with: %s", error->message);
+    if (error != NULL) {
+        g_error("Process errored with: %s", error->message);
     }
-    gint exit_status = g_subprocess_get_exit_status(process);
-    g_message("Exited with status: ", exit_status);
 }
 
-static char * _preprocess_input ( Mode *sw, const char *input )
+// It's a hacky way of making rofi show new window titles.
+extern void rofi_view_reload(void);
+
+static char* calc_preprocess_input(Mode* sw, const char* input)
 {
     g_autoptr(GError) error = NULL;
-    g_message("current input: %s", input);
-    MYPLUGINModePrivateData *pd = (MYPLUGINModePrivateData *) mode_get_private_data ( sw );
+    CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(sw);
 
-    const gchar * const argv[] = { "/usr/bin/qalc", "+u8", "-s", "update_exchange_rates 1days", input, NULL };
-    GSubprocess * process = g_subprocess_newv(argv, G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error);
+    const gchar* const argv[] = { "/usr/bin/qalc", "+u8", "-s", "update_exchange_rates 1days", input, NULL };
+    GSubprocess* process = g_subprocess_newv(argv, G_SUBPROCESS_FLAGS_STDOUT_PIPE | G_SUBPROCESS_FLAGS_STDERR_MERGE, &error);
 
-    if (error != NULL)
-    {
-        g_error ("Spawning child failed: %s", error->message);
+    if (error != NULL) {
+        g_error("Spawning child failed: %s", error->message);
     }
 
-    GInputStream * p_stdout = g_subprocess_get_stdout_pipe(process);
+    GInputStream* p_stdout = g_subprocess_get_stdout_pipe(process);
     g_autoptr(GString) stdout_str = g_string_new(NULL);
 
     char stdout_buf;
     while (g_input_stream_read(p_stdout, &stdout_buf, 1, NULL, &error) != 0) {
-        if (error != NULL)
-        {
-            g_error ("Error reading stdout: %s", error->message);
+        if (error != NULL) {
+            g_error("Error reading stdout: %s", error->message);
+        }
+
+        // We only want to get the first line.
+        if(stdout_buf == '\n') {
+            break;
         }
 
         g_string_append_c(stdout_str, stdout_buf);
+
     }
-    g_message("%s", stdout_str->str);
     pd->last_result = g_strdup(stdout_str->str);
 
-    g_subprocess_wait_async(process, NULL, process_cb, sw);
+    g_subprocess_wait_check_async(process, NULL, process_cb, sw);
 
-    return g_strdup ( input );
+    rofi_view_reload();
+    return g_strdup(input);
 }
 
+static char *calc_get_message ( const Mode *sw )
+{
+    CALCModePrivateData* pd = (CALCModePrivateData*)mode_get_private_data(sw);
+    if (g_strrstr(pd->last_result, "warning:") != NULL || g_strrstr(pd->last_result, "error:") != NULL) {
+        return g_markup_printf_escaped("<span foreground='PaleVioletRed'>%s</span>", pd->last_result);
+    }
+    return g_markup_printf_escaped("<b>%s</b>", pd->last_result);
+}
 
 Mode mode =
 {
     .abi_version        = ABI_VERSION,
     .name               = "calc",
-    .cfg_name_key       = "display-myplugin",
-    ._init              = myplugin_mode_init,
-    ._get_num_entries   = myplugin_mode_get_num_entries,
-    ._result            = myplugin_mode_result,
-    ._destroy           = myplugin_mode_destroy,
-    ._token_match       = myplugin_token_match,
-    ._get_display_value = _get_display_value,
-    ._get_message       = NULL,
-    ._get_completion    = NULL,
-    ._preprocess_input  = _preprocess_input,
+    .cfg_name_key       = "display-calc",
+    ._init              = calc_mode_init,
+    ._get_num_entries   = calc_mode_get_num_entries,
+    ._result            = calc_mode_result,
+    ._destroy           = calc_mode_destroy,
+    ._token_match       = calc_token_match,
+    ._get_display_value = calc_get_display_value,
+    ._get_message       = calc_get_message,
+    ._preprocess_input  = calc_preprocess_input,
     .private_data       = NULL,
     .free               = NULL,
 };
